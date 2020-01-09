ab11fef36948c23536fcfa3e7f5b7687df029376
Hasmukh <hasuparmar2494@gmail.com>
2020-01-09 05:21:51 -0600

    my very first

---
+++ git_test.php
@@ -0,0 +1,19 @@
+<?php
+if ($_POST) {
+    require 'vendor/autoload.php';
+    $connection = new MongoDB\Client("mongodb://127.0.0.1/");
+    $db = $connection->demoadd;
+    $id = new MongoDB\BSON\ObjectId($_POST['id']);
+    $db->emp->updateOne(
+        array('_id' => $id),
+        array(
+            '$set' => array(
+                'firstname' => $_POST["firstname"],
+                'lastname' => $_POST['lastname'],
+                'username' => $_POST['username'],
+            )
+        )
+    );
+    header("Location:add.php");
+}
+?><?php
